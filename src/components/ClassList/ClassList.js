caaf742aa87a26b081f9e21dc1f9d2668f88e40e
Harry Heckmann <harry@Harrys-MacBook-Pro.local>
2019-01-22 15:34:21 -0600

    adds finished afternoon-4

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+import Home from '../Home/Home'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(response =>{
+        this.setState({students: response.data})
+        console.log(response.data)
+      })
   }
 
   render() {
+    const students = this.state.students.map((e, i) => (
+      <Link to={`/student/${e.id}`}><h3 key={i}>{e.first_name} {e.last_name}</h3></Link>
+    ))
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
+        <Link to='/'><button className='btn'>Go Back</button></Link>
+  
       </div>
     )
   }
